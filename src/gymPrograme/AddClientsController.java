0890a69e0579bbd297778d6a34ffc92287381ff1
ayoubelmohamedi <ayoubprog61@gmail.com>
2020-04-25 23:54:03 +0000

    add delete Client button

--- src/gymPrograme/AddClientsController.java
+++ src/gymPrograme/AddClientsController.java
@@ -9,7 +9,6 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class AddClientsController {
-    
 
     @FXML
     private TextField namefield_id, identificationField_id, phoneNumber_id, ageField_id;
