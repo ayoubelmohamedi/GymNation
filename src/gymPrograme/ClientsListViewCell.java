3cfbc73f9d5fe2384e2badb993208890572af6ef
ayoubelmohamedi <ayoubprog61@gmail.com>
2020-04-26 17:33:33 +0000

    add trasaction to detail button

--- src/gymPrograme/ClientsListViewCell.java
+++ src/gymPrograme/ClientsListViewCell.java
@@ -1,12 +1,18 @@
 package gymPrograme;
 
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.geometry.Pos;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListCell;
 import javafx.scene.control.RadioButton;
 import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
 
 import java.io.IOException;
 
@@ -22,6 +28,9 @@ public class ClientsListViewCell extends ListCell<Clients> {
 
     @FXML
     private RadioButton payStatus_id;
+
+    @FXML
+    private Button detailButton_id;
 
     private FXMLLoader mLLoader;
 
@@ -47,6 +56,16 @@ public class ClientsListViewCell extends ListCell<Clients> {
             name_id.setText(clients.getName());
             idCard_id.setText(clients.getIdCard());
 
+            detailButton_id.setOnAction(actionEvent -> {
+                try {
+                    Stage stage = (Stage) detailButton_id.getScene().getWindow();
+                    Parent newParent = FXMLLoader.load(getClass().getResource("clientsInforPage.fxml"));
+                    stage.setScene(new Scene(newParent,600,400));
+                    stage.show();
+                }catch (Exception e){
+                    e.fillInStackTrace();
+                }
+            });
 
             setText(null);
             setGraphic(gridPane_id);
